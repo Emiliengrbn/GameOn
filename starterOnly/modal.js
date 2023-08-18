f12e58043d79f67f34bcde97b6cf2660612c8051
Emilien Gerbouin <emilien.gerbouin@gmail.com>
2023-08-18 10:00:51 +0200

    event click

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,23 +12,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
-let firstName = document.getElementById("first");
-let lastName = document.getElementById("last");
-let email = document.getElementById("email");
-let birthdate = document.getElementById("birthdate");
-let quantity = document.getElementById("quantity");
-let radioLocation = document.querySelectorAll("input[name='location']:checked");
-let condition = document.getElementById("checkbox1");
-let firstNameError = document.getElementById("firstNameErrorMsg");
-let lastNameError = document.getElementById("lastNameErrorMsg");
-let emailError = document.getElementById("emailErrorMsg");
-let birthdateError = document.getElementById("birthdateErrorMsg");
-let quantityError = document.getElementById("quantityErrorMsg");
-let locationError = document.getElementById("locationErrorMsg");
-let conditionError = document.getElementById("conditionErrorMsg");
-const errors = {};
-const regexMail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-const regexPhone = /^[0-9 +]+$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -48,6 +31,22 @@ function quitModal() {
 
 // Verification of the form
 
+let firstName = document.getElementById("first");
+let lastName = document.getElementById("last");
+let email = document.getElementById("email");
+let birthdate = document.getElementById("birthdate");
+let quantity = document.getElementById("quantity");
+let radioLocation = document.querySelectorAll("input[name='location']");
+let condition = document.getElementById("checkbox1");
+let firstNameError = document.getElementById("firstNameErrorMsg");
+let lastNameError = document.getElementById("lastNameErrorMsg");
+let emailError = document.getElementById("emailErrorMsg");
+let birthdateError = document.getElementById("birthdateErrorMsg");
+let quantityError = document.getElementById("quantityErrorMsg");
+let locationError = document.getElementById("locationErrorMsg");
+let conditionError = document.getElementById("conditionErrorMsg");
+let regexMail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+let regexPhone = /^[0-9 +]+$/;
 let regexOnlyLetter =
   /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
 let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
@@ -61,112 +60,15 @@ let locationCheck = false;
 let conditionCheck = true;
 let birthdateCheck = false;
 
-// // Test first name
-// firstName.addEventListener("change", () => {
-//   // if (
-//   //   regexOnlyLetter.test(firstName.value) === false ||
-//   //   regexName.test(firstName.value) === false
-//   // ) {
-//   //   firstNameError.innerHTML = "Veuillez renseigner un prénom valide";
-//   //   firstNameCheck = false;
-//   // } else {
-//   //   firstNameError.innerHTML = "";
-//   //   firstNameCheck = true;
-//   // }
-// });
-
-// // Test last name
-// lastName.addEventListener("change", () => {
-//   // if (
-//   //   regexOnlyLetter.test(lastName.value) === false ||
-//   //   regexName.test(lastName.value) === false
-//   // ) {
-//   //   lastNameError.innerHTML = "Veuillez renseigner un nom valide";
-//   //   lastNameCheck = false;
-//   // } else {
-//   //   lastNameError.innerHTML = "";
-//   //   lastNameCheck = true;
-//   // }
-// });
-
-// // Test email
-// email.addEventListener("change", () => {
-//   // if (regexEmail.test(email.value) === false) {
-//   //   emailError.innerHTML = "Veuillez renseigner un email valide";
-//   //   emailCheck = false;
-//   // } else {
-//   //   emailError.innerHTML = "";
-//   //   emailCheck = true;
-//   // }
-// });
-
-// // Test quantity
-// quantity.addEventListener("change", () => {
-//   // if (regexQuantity.test(quantity.value) === false) {
-//   //   quantityError.innerHTML = "Veuillez renseigner un nombre";
-//   //   quantityCheck = false;
-//   // } else {
-//   //   quantityError.innerHTML = "";
-//   //   quantityCheck = true;
-//   // }
-// });
-
-// // Test location
-
-// // Test condition
-// condition.addEventListener("change", () => {
-//   // if (!condition.checked) {
-//   //   conditionError.innerHTML =
-//   //     "Veuillez lire et accepter les condition d'utilisation";
-//   //   conditionCheck = false;
-//   // } else {
-//   //   conditionError.innerHTML = "";
-//   //   conditionCheck = true;
-//   // }
-// });
-
 // Verify all form
 let submitButton = document.getElementsByClassName("btn-submit");
 let form = document.querySelector("form");
 let confirmation = document.getElementsByClassName("confirmation_content");
-// submitButton[0].addEventListener("click", (e) => {
-//   e.preventDefault();
-//   if (!birthdate.value) {
-//     birthdateError.innerHTML = "Veuillez renseigner votre date de naissance";
-//     birthdateCheck = false;
-//   } else {
-//     birthdateError.innerHTML = "";
-//     birthdateCheck = true;
-//   }
-//   if (
-//     firstNameCheck &&
-//     lastNameCheck &&
-//     emailCheck &&
-//     quantityCheck &&
-//     conditionCheck &&
-//     birthdateCheck
-//   ) {
-//     form.style.display = "none";
-//     confirmation[0].style.display = "flex";
-//   } else {
-//     alert("merci de remplir tout les champs du formulaire");
-//   }
-// });
 
 submitButton[0].addEventListener("click", (e) => {
   e.preventDefault();
+  // Test firtName
   if (
-    firstNameCheck &&
-    lastNameCheck &&
-    emailCheck &&
-    quantityCheck &&
-    conditionCheck &&
-    birthdateCheck
-  ) {
-    form.style.display = "none";
-    confirmation[0].style.display = "flex";
-  } else if (
-    // Test firtName
     regexOnlyLetter.test(firstName.value) === false ||
     regexName.test(firstName.value) === false
   ) {
@@ -217,12 +119,16 @@ submitButton[0].addEventListener("click", (e) => {
   }
 
   // Test location
-  if (radioLocation.length === 0) {
-    locationError.innerHTML = "Veuillez selectionner une ville";
-    locationCheck = false;
-  } else {
-    locationError.innerHTML = "";
-    locationCheck = true;
+  for (let radio of radioLocation) {
+    if (!radio.checked) {
+      locationError.innerHTML = "Veuillez selectionner une ville";
+      locationCheck = false;
+    } else {
+      locationError.innerHTML = "";
+      locationCheck = true;
+
+      break;
+    }
   }
 
   // Test condition
@@ -233,5 +139,17 @@ submitButton[0].addEventListener("click", (e) => {
   } else {
     conditionError.innerHTML = "";
     conditionCheck = true;
+  }
+  if (
+    firstNameCheck &&
+    lastNameCheck &&
+    emailCheck &&
+    quantityCheck &&
+    conditionCheck &&
+    birthdateCheck &&
+    locationCheck
+  ) {
+    form.style.display = "none";
+    confirmation[0].style.display = "flex";
   }
 });
